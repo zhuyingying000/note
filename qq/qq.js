d822c300297774f6e7104dad02e8620d5b5a707c
zhuyingying000 <1798363558@qq.com>
2018-03-10 22:59:21 +0800

    保存

--- qq/qq.js
+++ qq/qq.js
@@ -1,4 +1,14 @@
-function load() {
+function goFast() {
+    var fast = document.querySelector('.tab');
+    var account = document.querySelector('#content');
+    fast.style['display'] = 'flex';
+    account.style['display'] = 'none';
+}
 
-console.log(123);
+
+function goAccount() {
+    var fast = document.querySelector('.tab');
+    var account = document.querySelector('#content');
+    fast.style['display'] = 'none';
+    account.style['display'] = 'flex';
 }
