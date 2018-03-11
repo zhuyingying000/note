23c9f63ee384dd2bf23ebd8faa656bcaaec28e7e
zhuyingying000 <1798363558@qq.com>
2018-03-11 21:29:34 +0800

    保存

---
+++ week/week.js
@@ -0,0 +1,29 @@
+function myButton() {
+    var x;
+    var d = new Date().getDay();
+
+    switch (d) {
+        case 0:
+            x = "今天是星期日";
+            break;
+        case 1:
+            x = "今天是星期一";
+            break;
+        case 2:
+            x = "今天是星期二";
+            break;
+        case 3:
+            x = "今天是星期三";
+            break;
+        case 4:
+            x = "今天是星期四";
+            break;
+        case 5:
+            x = "今天是星期五";
+            break;
+        case 6:
+            x = "今天是星期六";
+            break;
+    }
+    document.getElementById('demo').innerHTML=x;
+}
